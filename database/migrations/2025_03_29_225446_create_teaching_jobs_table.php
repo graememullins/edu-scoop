40270f13dae8bc5b8994554ae1267f5262bb0dc1
Graeme Mullins <mail@graememullins.com>
2025-04-03 22:12:16 +0100

    Updated table

--- database/migrations/2025_03_29_225446_create_teaching_jobs_table.php
+++ database/migrations/2025_03_29_225446_create_teaching_jobs_table.php
@@ -41,8 +41,6 @@ return new class extends Migration
             $table->string('pfa')->nullable();
             $table->string('region')->nullable(); // Region of the job
             $table->string('country')->nullable(); // Default to UK
-            $table->string('nuts')->nullable(); // Nomenclature of Territorial Units for Statistics
-            $table->string('pfa')->nullable(); // Public Funding Agency
             $table->string('post_code')->nullable(); // Postal code
             $table->boolean('is_scraped')->default(false); // Scrape status
             $table->boolean('keyword_checked')->default(false);
