65959128d54b1c384ed6779bb46439aa03e4f4be
Graeme Mullins <mail@graememullins.com>
2025-04-05 19:59:45 +0100

    Added keep alive route

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Log;
+
+Route::get('/keep-alive', function () {
+    Log::info('Keep-alive ping received at ' . now()->toDateTimeString());
+
+    return response('OK', 200);
+});
